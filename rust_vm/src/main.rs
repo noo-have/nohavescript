e2589f30f3219bdcb0cc1475cde0506a8b776051
noo-have <2485628846@qq.com>
2023-03-27 16:21:55 +0800

    简易的repl实现

--- rust_vm/src/main.rs
+++ rust_vm/src/main.rs
@@ -7,8 +7,94 @@ mod tokenizer;
 mod translate;
 mod type_check;
 mod vm;
+fn main() {
+    debug!("{}", std::mem::size_of::<NsType>());
+    let mut c = std::env::args();
+    if c.len() == 1 {
+        repl().unwrap();
+    } else {
+        let path = c.nth(1).unwrap();
+        let result = std::fs::read_to_string(path).unwrap();
+        let mut parser = Parser::new(&result);
+        let reg = Regex::new("-(.*)").unwrap();
+        // 第一个参数为""或当前路径
+        for arg in c {
+            let arg = reg
+                .captures(&arg)
+                .unwrap()
+                .unwrap()
+                .get(1)
+                .unwrap()
+                .as_str();
+            debug!("{:?}", arg);
+            match arg {
+                "help" => {}
+                "run" => {
+                    // let vm = vm::VM::new();
+                    parser.set_gen_bytecode(true);
+                    parser.start();
+                    // vm::run(bytecode)
+                }
+                "translate" => {
+                    parser.set_translate(true);
+                    parser.start();
+                }
+                "bytecode" => {
+                    parser.set_gen_bytecode(true);
+                    parser.start();
+                    // write_file(bytecode,path)
+                }
+                _ => panic!(),
+            }
+        }
+    }
+}
+use std::io::Write;
 
-fn main() {}
+use fancy_regex::Regex;
+use type_check::NsType;
+
+use crate::parser::Parser;
+
+fn repl() -> Result<(), String> {
+    let mut parser = Parser::new("");
+    loop {
+        let line = read_line()?;
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        match respond(line, &mut parser) {
+            Ok(quit) => {
+                if quit {
+                    break;
+                }
+            }
+            Err(err) => {
+                write!(std::io::stdout(), "{err}").map_err(|e| e.to_string())?;
+                std::io::stdout().flush().map_err(|e| e.to_string())?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn respond(line: &str, parser: &mut Parser) -> Result<bool, String> {
+    // let args = shlex::split(line).ok_or("error: Invalid quoting")?;
+    parser.set_source_code(line);
+    parser.start();
+    Ok(false)
+}
+
+fn read_line() -> Result<String, String> {
+    write!(std::io::stdout(), "$ ").map_err(|e| e.to_string())?;
+    std::io::stdout().flush().map_err(|e| e.to_string())?;
+    let mut buffer = String::new();
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .map_err(|e| e.to_string())?;
+    Ok(buffer)
+}
 
 #[cfg(test)]
 mod test_all {
@@ -20,7 +106,10 @@ mod test_all {
         }
     }
     mod vm_test {
-        use crate::vm::{ByteCode, NsValue, VM};
+        use crate::{
+            debug,
+            vm::{ByteCode, NsValue, VM},
+        };
         use std::time;
         #[test]
         fn vm() {
@@ -44,10 +133,10 @@ mod test_all {
             ];
             let p = time::Instant::now();
             vm.run(&bytecode_list);
-            println!("vm花了{:?}", time::Instant::now() - p);
+            debug!("vm花了{:?}", time::Instant::now() - p);
             let p = time::Instant::now();
             for _ in 0..10000000 {}
-            println!("rust花了{:?}", time::Instant::now() - p);
+            debug!("rust花了{:?}", time::Instant::now() - p);
         }
     }
 }
