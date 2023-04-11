51d6725548f21a9b817c54cb74e1eae47eaf31f0
noo-have <2485628846@qq.com>
2023-04-11 17:14:26 +0800

    i32类型改成u32类型; String类型改成'static str类型,会导致内存泄漏; 添加恐慌模式的实现,将Result<_,String>都换成Result<_,ParseError>; 添加对函数类型字面量的解析

--- rust_vm/src/parser.rs
+++ rust_vm/src/parser.rs
@@ -1,7 +1,10 @@
 //! 解析器模块
 use std::collections::HashMap;
 
-use crate::tokenizer::{Token, TokenType, Tokenizer};
+use crate::{
+    error,
+    tokenizer::{Token, TokenType, Tokenizer},
+};
 /// 语句
 #[cfg_attr(debug_assertions, derive(Debug))]
 pub enum Stat {
@@ -37,7 +40,7 @@ pub enum Stat {
         /// 是不是异步函数?
         is_async: bool,
         /// 函数名称
-        name: String,
+        name: &'static str,
         /// 参数列表,由(模式,可选的类型标注)元组构成的不定长数组
         args: Vec<(Pattern, Option<TypeLiteral>)>,
         /// 返回值的可选的类型标注
@@ -62,7 +65,7 @@ pub enum Stat {
 #[cfg_attr(debug_assertions, derive(Debug))]
 pub enum Pattern {
     /// `e`
-    Id(String),
+    Id(&'static str),
     /// `_`
     Ignore,
     /// ```
@@ -82,15 +85,15 @@ pub enum Pattern {
     /// {a,b:c}
     ///
     /// ```
-    ObjectMatch(HashMap<String, Pattern>),
+    ObjectMatch(HashMap<&'static str, Pattern>),
     /// ```
     ///
     /// E::a | E::b(_)
     ///
     /// ```
     EnumMember {
-        enum_name: String,
-        enum_member: String,
+        enum_name: &'static str,
+        enum_member: &'static str,
         pattern_group: Vec<Pattern>,
     },
 }
@@ -102,9 +105,9 @@ pub enum Expr {
     ///
     /// ```
     字面量 {
-        value: String,
-        line: i32,
-        column: i32,
+        value: &'static str,
+        line: u32,
+        column: u32,
     },
     /// ```
     ///
@@ -113,49 +116,49 @@ pub enum Expr {
     /// ```
     元组 {
         elements: Vec<Expr>,
-        line: i32,
-        column: i32,
+        line: u32,
+        column: u32,
     },
     /// ```
     ///
     /// ()
     ///
     /// ```
-    单元 { line: i32, column: i32 },
+    单元 { line: u32, column: u32 },
     /// ```
     ///
     /// a
     ///
     /// ````
     标识符 {
-        value: String,
-        line: i32,
-        column: i32,
+        value: &'static str,
+        line: u32,
+        column: u32,
     },
     /// `1+1`
     二元表达式 {
-        operator: String,
+        operator: &'static str,
         left: Box<Expr>,
         right: Box<Expr>,
-        line: i32,
-        column: i32,
+        line: u32,
+        column: u32,
     },
     /// `d` = `3`
     赋值表达式 {
-        operator: String,
+        operator: &'static str,
         left: Box<Expr>,
         right: Box<Expr>,
-        line: i32,
-        column: i32,
+        line: u32,
+        column: u32,
     },
     /// `{1+1}`
     块表达式 {
         body: Vec<Stat>,
-        line: i32,
-        column: i32,
+        line: u32,
+        column: u32,
     },
     /// `{a}` | `{a,l:1}`
-    对象表达式 { property: Vec<(String, Expr)> },
+    对象表达式 { property: Vec<(&'static str, Expr)> },
 }
 
 /// 类型字面量
@@ -169,94 +172,73 @@ pub enum TypeLiteral {
     /// `T<U>`
     Generic {
         /// 类型名称
-        name: String,
+        name: &'static str,
         /// 类型参数
         type_args: Vec<TypeLiteral>,
     },
     /// 无参数的类型字面量
     ///
     /// `T`
-    Normal(String),
+    Normal(&'static str),
     /// 元组类型字面量
     ///
     /// `(T,U)`
     Tuple(Vec<TypeLiteral>),
     /// 字面量 `_`
     Infer,
-    ///
-    ///
-    ///
-    ObjLiteral(HashMap<String, TypeLiteral>),
+    /// 对象
+    ObjLiteral(HashMap<&'static str, TypeLiteral>),
+    /// 函数
+    Fn(Vec<TypeLiteral>),
 }
 /// 解析器
 pub struct Parser {
-    /// 行,紧跟分词器
-    pub line: i32,
-    /// 列,紧跟分词器
-    pub column: i32,
     /// 分词器
     pub tokenizer: Tokenizer,
     /// 当前token
     pub now_token: Token,
     /// 语法树
-    pub ast: Vec<Stat>,
+    pub ast: Vec<Result<Stat, error::ParseError>>,
     /// 上一个token
     pub previous_token: Token,
-    pub gen_bytecode: bool,
-    pub translate: bool,
 }
 impl Parser {
     /// 启动解析
-    pub fn start(&mut self) -> Result<(), String> {
+    pub fn start(&mut self) {
         if let Some(token) = self.get_next_token() {
             if let TokenType::结束 = token.token_type {
-                return Ok(());
+                return;
             }
             self.now_token = token;
-            self.line = self.now_token.line;
-            self.column = self.now_token.column;
-            self.parse()?;
+            self.parse();
         }
-        Ok(())
     }
-    /// 让编译器生成字节码
-    pub fn set_gen_bytecode(&mut self, gen_bytecode: bool) {
-        self.gen_bytecode = gen_bytecode
-    }
-    /// 让编译器转译js
-    pub fn set_translate(&mut self, translate: bool) {
-        self.translate = translate
-    }
-    /// 设置新的ns代码,重置分词器
+    /// 设置新的ns代码,在分词器源码末尾添加
     pub fn set_source_code(&mut self, source_code: &str) {
-        self.tokenizer = Tokenizer::new(source_code)
+        self.tokenizer.source_code += source_code;
     }
     pub fn new(str: &str) -> Self {
         Parser {
-            column: 1,
-            line: 1,
             tokenizer: Tokenizer::new(str),
             now_token: Token {
                 token_type: TokenType::空格,
-                value: " ".to_string(),
+                value: " ",
                 line: 0,
                 column: 0,
             },
             ast: vec![],
             previous_token: Token {
                 token_type: TokenType::空格,
-                value: " ".to_string(),
+                value: " ",
                 line: 0,
                 column: 0,
             },
-            gen_bytecode: false,
-            translate: false,
         }
     }
     /// 解析入口
-    fn parse(&mut self) -> Result<(), String> {
+    fn parse(&mut self) {
         loop {
-            let stat = self.parse_stat()?;
+            let stat = self.parse_stat();
             self.ast.push(stat);
             // 匹配掉 `;`
             self.match_token_value(&[";"]);
@@ -264,19 +246,16 @@ impl Parser {
                 break;
             }
         }
-        Ok(())
     }
     /// 拿取下一个token,并且将parser的各项数据与新token对齐
     fn eat_token(&mut self) {
         self.previous_token = self.now_token.clone();
-        self.now_token = self.get_next_token().unwrap_or_else(|| Token {
+        self.now_token = self.get_next_token().unwrap_or(Token {
             token_type: TokenType::结束,
-            value: " ".to_string(),
+            value: " ",
             line: self.now_token.line,
             column: self.now_token.column,
         });
-        self.line = self.now_token.line;
-        self.column = self.now_token.column;
     }
     /// 检查当前token的值,然后推进
     fn match_token_value(&mut self, value_arr: &[&str]) -> bool {
@@ -324,32 +303,51 @@ impl Parser {
     fn get_previous_token(&self) -> &Token {
         &self.previous_token
     }
-    /// 行为与match_token_value一致,不同之处在于如果匹配不成功是一个错误
-    fn consume_token_value(&mut self, value_arr: &[&str], message: &str) {
+    /// 行为与match_token_value一致,不同之处在于如果匹配不成功就进入恐慌模式
+    fn consume_token_value(
+        &mut self,
+        value_arr: &[&str],
+        message: &str,
+    ) -> Result<(), error::ParseError> {
         if value_arr.iter().any(|c| *c == self.now_token.value) {
             self.eat_token();
+            Ok(())
         } else {
-            todo!()
+            Err(self.panic_mode(message))
         }
     }
-    /// 行为与match_token_type一致,不同之处在于如果匹配不成功是一个错误
-    fn consume_token_type(&mut self, token_type: TokenType, message: &str) {
+    /// 行为与match_token_type一致,不同之处在于如果匹配不成功就进入恐慌模式
+    fn consume_token_type(
+        &mut self,
+        token_type: TokenType,
+        message: &str,
+    ) -> Result<(), error::ParseError> {
         if token_type == self.now_token.token_type {
             self.eat_token();
+            Ok(())
         } else {
-            // 错误
-            todo!()
+            Err(self.panic_mode(message))
         }
     }
+    /// 进入恐慌模式
+    ///
+    /// 开始吞token,直到now_token是下个语句开头,或者 `;`
+    fn panic_mode(&mut self, message: &str) -> error::ParseError {
+        self.eat_token();
+        while !self.check_token_value(&[";", "{", "let"]) {
+            self.eat_token();
+        }
+        error::syntax_error(message, self.now_token.line, self.now_token.column)
+    }
     /// 解析语句的入口
-    fn parse_stat(&mut self) -> Result<Stat, String> {
+    fn parse_stat(&mut self) -> Result<Stat, error::ParseError> {
         if self.match_token_value(&["let"]) {
             let pattern = self.parse_pattern()?;
             let mut type_annotation = None;
             if self.match_token_value(&[":"]) {
                 type_annotation = Some(self.parse_type_literal()?);
             }
-            self.consume_token_value(&["="], "缺失等于号");
+            self.consume_token_value(&["="], "缺失等于号")?;
             Ok(Stat::变量声明语句 {
                 is_const: false,
                 pattern,
@@ -362,7 +360,7 @@ impl Parser {
             todo!()
         } else if self.match_token_value(&["type"]) {
             let type_name = self.parse_type_literal()?;
-            self.consume_token_value(&["="], "缺失等于号");
+            self.consume_token_value(&["="], "缺失等于号")?;
             Ok(Stat::类型声明语句(
                 type_name,
                 self.parse_type_literal()?,
@@ -370,9 +368,9 @@ impl Parser {
         } else if self.match_token_value(&["enum"]) {
             todo!()
         } else if self.match_token_value(&["fn"]) {
-            self.consume_token_type(TokenType::标识符, "需要函数名");
-            let name = self.get_previous_token().value.to_string();
-            self.consume_token_type(TokenType::左括号, "缺失左括号");
+            self.consume_token_type(TokenType::标识符, "需要函数名")?;
+            let name = self.get_previous_token().value;
+            self.consume_token_type(TokenType::左括号, "缺失左括号")?;
             let mut args = vec![];
             while !self.match_token_type(TokenType::右括号) {
                 let pat = self.parse_pattern()?;
@@ -380,7 +378,7 @@ impl Parser {
                     self.match_token_value(&[":"]);
                     args.push((pat, Some(self.parse_type_literal()?)));
                 } else {
-                    self.consume_token_value(&[","], "缺失逗号");
+                    self.consume_token_value(&[","], "缺失逗号")?;
                     args.push((pat, None));
                 }
             }
@@ -406,26 +404,26 @@ impl Parser {
     /// 解析类型字面量,例如:
     ///
     /// `S` | `S<T>` | `S<T,U>` | `S<T<U>,P>` | `(S,)` | `(S<T>,U)`
-    fn parse_type_literal(&mut self) -> Result<TypeLiteral, String> {
+    fn parse_type_literal(&mut self) -> Result<TypeLiteral, error::ParseError> {
         if self.match_token_value(&["("]) {
             if self.match_token_value(&[")"]) {
                 Ok(TypeLiteral::Tuple(vec![]))
             } else if self.check_token_type(TokenType::标识符) {
                 let mut p = vec![self.parse_type_literal()?];
                 while !self.match_token_value(&[")"]) {
-                    self.consume_token_value(&[","], "");
+                    self.consume_token_value(&[","], "")?;
                     p.push(self.parse_type_literal()?);
                 }
                 Ok(TypeLiteral::Tuple(p))
             } else {
-                Err("不合法的类型标注".to_string())
+                Err(self.panic_mode("不合法的类型标注"))
             }
         } else if self.match_token_type(TokenType::标识符) {
-            let name = self.get_previous_token().value.clone();
+            let name = self.get_previous_token().value;
             if self.match_token_value(&["<"]) {
                 let mut type_arg = vec![self.parse_type_literal()?];
                 while !self.match_token_value(&[">"]) {
-                    self.consume_token_value(&[","], "");
+                    self.consume_token_value(&[","], "")?;
                     type_arg.push(self.parse_type_literal()?);
                 }
                 Ok(TypeLiteral::Generic {
@@ -439,33 +437,39 @@ impl Parser {
             Ok(TypeLiteral::Infer)
         } else if self.match_token_value(&["{"]) {
             if self.match_token_value(&["}"]) {
-                Err("使用 `()` 来代替".to_string())
+                Err(self.panic_mode("使用 `()` 来代替"))
             } else {
                 let mut field_table = HashMap::new();
                 while !self.match_token_value(&["}"]) {
-                    self.consume_token_type(TokenType::标识符, "需要标识符");
+                    self.consume_token_type(TokenType::标识符, "需要标识符")?;
 
-                    let field_name = self.get_previous_token().value.clone();
-                    self.consume_token_value(&[":"], "需要 `:`");
+                    let field_name = self.get_previous_token().value;
+                    self.consume_token_value(&[":"], "需要 `:`")?;
 
                     field_table.insert(field_name, self.parse_type_literal()?);
                     self.match_token_value(&[";", ","]);
                 }
                 Ok(TypeLiteral::ObjLiteral(field_table))
             }
+        } else if self.match_token_value(&["Fn"]) {
+            let mut result = vec![self.parse_type_literal()?];
+            while self.match_token_value(&["->"]) {
+                result.push(self.parse_type_literal()?)
+            }
+            Ok(TypeLiteral::Fn(result))
         } else {
-            Err("不合法的类型标注".to_string())
+            Err(self.panic_mode("不合法的类型标注"))
         }
     }
     /// 解析 "模式"
-    fn parse_pattern(&mut self) -> Result<Pattern, String> {
+    fn parse_pattern(&mut self) -> Result<Pattern, error::ParseError> {
         if self.match_token_value(&["("]) {
             if self.match_token_value(&[")"]) {
-                return Err("无意义的模式,用 `_` 代替".to_string());
+                return Err(self.panic_mode("无意义的模式,用 `_` 代替"));
             }
             let mut pattern_arr = vec![self.parse_pattern()?];
             while !self.match_token_value(&[")"]) {
-                self.consume_token_value(&[","], "缺失逗号");
+                self.consume_token_value(&[","], "缺失逗号")?;
                 if self.match_token_value(&[")"]) {
                     break;
                 }
@@ -474,25 +478,25 @@ impl Parser {
             Ok(Pattern::TupleMatch(pattern_arr))
         } else if self.match_token_value(&["{"]) {
             if self.match_token_value(&["}"]) {
-                return Err("无意义的模式,用 `_` 代替".to_string());
+                return Err(self.panic_mode("不合法的类型标注"));
             }
             let mut pattern_map = HashMap::new();
 
             while !self.match_token_value(&["}"]) {
-                self.consume_token_type(TokenType::标识符, "a");
-                let name = self.get_previous_token().value.clone();
+                self.consume_token_type(TokenType::标识符, "a")?;
+                let name = self.get_previous_token().value;
                 if self.match_token_value(&[":"]) {
                     pattern_map.insert(name, self.parse_pattern()?);
                 } else if self.match_token_value(&[","]) {
-                    pattern_map.insert(name.clone(), Pattern::Id(name));
+                    pattern_map.insert(name, Pattern::Id(name));
                 }
             }
             Ok(Pattern::ObjectMatch(pattern_map))
         } else if self.match_token_type(TokenType::标识符) {
-            let name = self.get_previous_token().value.clone();
+            let name = self.get_previous_token().value;
             if self.match_token_value(&["::"]) {
-                self.consume_token_type(TokenType::标识符, "缺失枚举成员名");
-                let member_name = self.get_previous_token().value.clone();
+                self.consume_token_type(TokenType::标识符, "缺失枚举成员名")?;
+                let member_name = self.get_previous_token().value;
                 if self.match_token_value(&["("]) {
                     if self.match_token_value(&[")"]) {
                         return Ok(Pattern::EnumMember {
@@ -503,7 +507,7 @@ impl Parser {
                     }
                     let mut pattern = vec![self.parse_pattern()?];
                     while !self.match_token_value(&[")"]) {
-                        self.consume_token_value(&[","], "缺失逗号");
+                        self.consume_token_value(&[","], "缺失逗号")?;
                         if self.match_token_value(&[")"]) {
                             break;
                         }
@@ -529,16 +533,16 @@ impl Parser {
         }
     }
     /// 解析表达式的入口
-    fn parse_expr(&mut self) -> Result<Expr, String> {
+    fn parse_expr(&mut self) -> Result<Expr, error::ParseError> {
         self.assignment()
     }
     /// 乘除表达式
-    fn factor(&mut self) -> Result<Expr, String> {
+    fn factor(&mut self) -> Result<Expr, error::ParseError> {
         let mut expr: Expr = self.primary_expr()?;
         let column = self.get_previous_token().column;
         let line = self.get_previous_token().line;
         while self.match_token_value(&["*", "/"]) {
-            let operator = (self.get_previous_token().value).to_string();
+            let operator = self.get_previous_token().value;
             let right = self.primary_expr()?;
             expr = Expr::二元表达式 {
                 operator,
@@ -551,7 +555,7 @@ impl Parser {
         Ok(expr)
     }
     /// 加减表达式
-    fn term(&mut self) -> Result<Expr, String> {
+    fn term(&mut self) -> Result<Expr, error::ParseError> {
         // 加减法优先级很低
         // 首先调用乘法解析,之后的表达式都会递归解析
         let mut expr = self.factor()?;
@@ -560,7 +564,7 @@ impl Parser {
         // 解析完后轮到自己
         while self.match_token_value(&["+", "-"]) {
             // match消耗了运算符,所以用 getPreviousToken 拿回来
-            let operator = (self.get_previous_token().value).to_string();
+            let operator = self.get_previous_token().value;
             // 右边再次递归解析
             let right = self.factor()?;
             // 它本身是新表达式的左侧
@@ -578,13 +582,13 @@ impl Parser {
         Ok(expr)
     }
     /// 赋值表达式
-    fn assignment(&mut self) -> Result<Expr, String> {
+    fn assignment(&mut self) -> Result<Expr, error::ParseError> {
         let mut expr = self.term();
         let column = self.get_previous_token().column;
         let line = self.get_previous_token().line;
         while self.match_token_value(&["="]) {
             let right = self.assignment();
-            let operator = self.get_previous_token().value.to_string();
+            let operator = self.get_previous_token().value;
             expr = Ok(Expr::赋值表达式 {
                 operator,
                 left: Box::new(expr?),
@@ -596,7 +600,7 @@ impl Parser {
         expr
     }
     /// 块表达式
-    fn block_expr(&mut self) -> Result<Expr, String> {
+    fn block_expr(&mut self) -> Result<Expr, error::ParseError> {
         let Token {
             mut line,
             mut column,
@@ -612,7 +616,7 @@ impl Parser {
         Ok(Expr::块表达式 { body, line, column })
     }
     /// ### 优先级最高,基本表达式
-    fn primary_expr(&mut self) -> Result<Expr, String> {
+    fn primary_expr(&mut self) -> Result<Expr, error::ParseError> {
         if self.match_token_type(TokenType::数字字面量)
             || self.match_token_type(TokenType::布尔值)
             || self.match_token_type(TokenType::字符串)
@@ -624,7 +628,7 @@ impl Parser {
                 ..
             } = &self.get_previous_token();
             Ok(Expr::字面量 {
-                value: value.to_string(),
+                value,
                 column: *column,
                 line: *line,
             })
@@ -636,7 +640,7 @@ impl Parser {
                 ..
             } = &self.get_previous_token();
             Ok(Expr::标识符 {
-                value: value.to_string(),
+                value,
                 column: *column,
                 line: *line,
             })
@@ -659,7 +663,7 @@ impl Parser {
                     column,
                 })
             } else {
-                self.consume_token_type(TokenType::右括号, "缺失右括号");
+                self.consume_token_type(TokenType::右括号, "缺失右括号")?;
                 Ok(expr)
             }
         } else if self.check_token_value(&["{"]) {
